e5a01df1f2f039ca554b4e534e50fb24a023c85d
Matthew Rumschlag <mrumschlag@gmail.com>
2017-09-27 19:24:24 -0400

    updated

--- index.js
+++ index.js
@@ -1,24 +1,14 @@
 'use strict';
-var express        = require('express'),
-    bodyParser     = require('body-parser'),
-    http           = require('http'),
-    app            = express();
+var express = require('express')
+var app = express()
 
-app.use(bodyParser.json());
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/public'))
 
-// set port
-app.set('port', process.env.PORT || 8080);
-
-// create a health check endpoint
-app.get('/health', function(req, res) {
-  res.send('okay');
-});
-
-app.post('/fb',  function(req,res){
-  res.send(req.body)
+app.get('/', function(request, response) {
+  response.send('Hello World!')
 })
 
-// start the server
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
